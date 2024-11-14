9edf8006b8226df8970af4eda2889eb2c20cfd01
ishikaalunawat <ishikaa@stanford.edu>
2024-11-14 01:47:35 +0000

    edits - 13 Nove

--- datasets.py
+++ datasets.py
@@ -6,6 +6,7 @@ import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import numpy as np
+import glob
 
 def collate_fn(batch):
     images = []
@@ -13,11 +14,11 @@ def collate_fn(batch):
     bboxes = []
 
     for item in batch:
-        images.append(item[0])  # item[0]: image tensor
-        labels.append(item[1])  # item[1]: label tensor
-        bboxes.append(item[2])  # item[2]: list of bounding boxes
+        images.append(item[0])  # image
+        labels.append(item[1])  # label
+        bboxes.append(item[2])  # bounding boxes
 
-    # Stack images and labels into tensors
+    # stack images & labels into tensors
     images = torch.stack(images, dim=0)
     labels = torch.stack(labels, dim=0)
 
@@ -25,7 +26,7 @@ def collate_fn(batch):
     return images, labels, bboxes
 
 def get_train_transform():
-    # Define data transformations for training and validation/testing
+    # transformations for training and validation/testing
     train_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.RandomHorizontalFlip(),
@@ -125,3 +126,69 @@ class UnderwaterCreaturesMultiLabelDataset(Dataset):
             bbox_list_converted.append([xmin, ymin, xmax, ymax, class_label])
 
         return image, labels, bbox_list_converted
+
+class YOLODataset(Dataset):
+    def __init__(self, root_dir, split='train', num_classes=7):
+        self.root_dir = root_dir
+        self.split = split
+        self.num_classes = num_classes
+
+        self.images_dir = os.path.join(root_dir, split, 'images')
+        self.labels_dir = os.path.join(root_dir, split, 'labels')
+
+        self.image_files = glob.glob(os.path.join(self.images_dir, '*.jpg')) 
+        self.samples = []
+
+        for image_file in self.image_files:
+            image_path = image_file
+            label_file = os.path.splitext(os.path.basename(image_file))[0] + '.txt'
+            label_path = os.path.join(self.labels_dir, label_file)
+            labels = torch.zeros(self.num_classes)
+            bboxes = []
+            if os.path.exists(label_path):
+                with open(label_path, 'r') as f:
+                    for line in f:
+                        parts = line.strip().split()
+                        if len(parts) == 5:
+                            class_id, x_center, y_center, width, height = map(float, parts)
+                            labels[int(class_id)] = 1  # Set presence of class
+                            bboxes.append([x_center, y_center, width, height, int(class_id)])
+            self.samples.append((image_path, labels, bboxes))
+
+        # Define transformations for YOLO dataset
+        if split == 'train':
+            self.transform = A.Compose([
+                A.Resize(224, 224),
+                A.HorizontalFlip(p=0.5),
+                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ToTensorV2()
+            ], bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+        else:
+            self.transform = A.Compose([
+                A.Resize(224, 224),
+                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ToTensorV2()
+            ], bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        image_path, labels, bboxes = self.samples[idx]
+        image = np.array(Image.open(image_path).convert('RGB'))
+
+        # Prepare bounding boxes and class labels
+        bbox_list = []
+        class_labels = []
+        for bbox in bboxes:
+            x_center, y_center, width, height, class_id = bbox
+            bbox_list.append([x_center, y_center, width, height])
+            class_labels.append(int(class_id))
+
+        # Apply transformations
+        transformed = self.transform(image=image, bboxes=bbox_list, class_labels=class_labels)
+        image = transformed['image']
+        bbox_list = transformed['bboxes']
+        class_labels = transformed['class_labels']
+
+        return image, labels, bbox_list
